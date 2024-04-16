35fe69ff69ecd74e5c7b555cd2d787451f09c2fb
LuisDogo <luisdogoms@gmail.com>
2024-04-16 14:28:44 -0600

    mas main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 from regresion_normal import normal as regnormal
+from gradiente import reg_gradiente as reggrad
 #from sklearn import linear_model
 
 def main():
     regnormal()
+    reggrad()
 
 if __name__ == "__main__":
     main()
