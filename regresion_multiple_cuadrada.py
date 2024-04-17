e9036ac0f9440cb4a879f88ab609262f0053f436
LuisDogo <luisdogoms@gmail.com>
2024-04-16 22:47:56 -0600

    murder on the dancefloor

--- regresion_multiple_cuadrada.py
+++ regresion_multiple_cuadrada.py
@@ -6,17 +6,7 @@ def regresion_multiple(X, b):
     sigma0 = np.empty([m,1])
     for column in range(m):
         sigma0[column] = X[:, column].sum()
-    sigma0 = np.insert(sigma0, 0, 1)
-    print(sigma0) 
-    A = np.outer(sigma0,sigma0)
+    sigma1 = np.insert(sigma0, 0, 1)
+    A = np.outer(sigma1,sigma1)
     A[0,0] = m
-    #A = np.outer(sigma0,sigma0)    
-    print(sigma0) 
-    print(A)
-    print("res")
     return np.dot(A,b)
-
-
-X = np.array([[1, 2, 1], [3, 4, 3], [5, 6, 3], [7, 8, 3]])
-y = np.array([3, 7, 11, 15])
-print(regresion_multiple(X,y))
