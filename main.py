94d181bd7bc2fef62e866343fbd5a82498efba9a
LuisDogo <luisdogoms@gmail.com>
2024-04-16 20:44:48 -0600

    semeolvidoactualizar

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 from regresion_normal import normal as regnormal
 from gradiente import reg_gradiente as reggrad
 from regresion_multiple_cuadrada import regresion_multiple as regmul
-#from sklearn import linear_model
+from sklearn import linear_model
+
 
 def main():
     regnormal()
--- regresion_multiple_cuadrada.py
+++ regresion_multiple_cuadrada.py
@@ -1,17 +1,22 @@
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 def regresion_multiple(X, b):
-    sigma0 = np.empty([len(X),1])
-    for column in range(len(X)):
-        sigma0[column] = sum(X[:, column])
-    sigma1 = np.insert(sigma0, 0, 1)
-    #A = np.outer(sigma1,sigma1)
-    A = np.outer(sigma0,sigma0)    
+    m = X.shape[1]
+    sigma0 = np.empty([m,1])
+    for column in range(m):
+        sigma0[column] = X[:, column].sum()
+    sigma0 = np.insert(sigma0, 0, 1)
+    print(sigma0) 
+    A = np.outer(sigma0,sigma0)
+    A[0,0] = m
+    #A = np.outer(sigma0,sigma0)    
+    print(sigma0) 
     print(A)
     print("res")
     return np.dot(A,b)
 
 
-a = np.array([[3, 7, 5],[1, 3, 0]])
-b = np.array([1, 3])
-print(regresion_multiple(a,b))
+X = np.array([[1, 2, 1], [3, 4, 3], [5, 6, 3], [7, 8, 3]])
+y = np.array([3, 7, 11, 15])
+print(regresion_multiple(X,y))
