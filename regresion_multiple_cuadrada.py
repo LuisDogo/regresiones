1556232831b0e0e4b3f33f2bfcbccb3a299dd560
LuisDogo <luisdogoms@gmail.com>
2024-04-16 19:09:24 -0600

    sigma

--- regresion_multiple_cuadrada.py
+++ regresion_multiple_cuadrada.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 def regresion_multiple(X, b):
-    # y  = mx +b == y = b0 + b1x + b2x
-    sigma = np.empty([len(X),1])
+    sigma0 = np.empty([len(X),1])
     for column in range(len(X)):
-        sigma[column] = sum(X[:, column])
-    A = np.outer(sigma.T,sigma)
+        sigma0[column] = sum(X[:, column])
+    sigma1 = np.insert(sigma0, 0, 1)
+    #A = np.outer(sigma1,sigma1)
+    A = np.outer(sigma0,sigma0)    
+    print(A)
+    print("res")
     return np.dot(A,b)
 
 
-a = np.array([[1, 3, 5],[1, 3, 5]])
+a = np.array([[3, 7, 5],[1, 3, 0]])
 b = np.array([1, 3])
 print(regresion_multiple(a,b))
